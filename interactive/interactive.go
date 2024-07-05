da501da786941236211a23aa11a484d757e00178
pan_ding_rong <panseng.dr@qq.com>
2024-07-05 16:22:18 +0800

    perf: 调整休眠时间提高开启投屏的效率

--- interactive/interactive.go
+++ interactive/interactive.go
@@ -30,7 +30,7 @@ func NewController(tv *soapcalls.TVPayload, server *httphandlers.HTTPserver) *Co
 }
 
 func (c *Controller) Init() error {
-	time.Sleep(300 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 	// Sending the Play1 action sooner may result
 	// in a panic error since we need to properly
 	// initialize the tcell window.
